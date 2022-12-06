85f15d3388b3da86d7f1c748c1b71cbbece68300
Ankit Seth <iam4nkit@gmail.com>
2022-12-07 00:38:10 +0530

    added ui for homepage and added other screens

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import Home from "./screens/Home";
+import Quiz from "./screens/Quiz";
+import Result from "./screens/Result";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
+      {/* <Quiz /> */}
+      {/* <Result /> */}
     </View>
   );
 }
@@ -13,8 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#662E9B",
+    padding: 40,
   },
 });
---
+++ screens/Home.js
@@ -0,0 +1,70 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.logo} source={require("../assets/logo.png")} />
+        <Text style={styles.title}>Quizzlet</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={{ color: "#fff", fontSize: 36 }}>Let's play!</Text>
+        <Text style={{ color: "#fff", fontSize: 22 }}>Are you ready?</Text>
+      </View>
+      <View
+        style={{
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: "#fff", fontSize: 24, fontWeight: "600" }}>
+            PLAY
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  header: {
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#662E9B",
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  title: {
+    fontSize: 46,
+    fontFamily: "Roboto",
+    fontWeight: "bolder",
+    color: "#F9C80E",
+  },
+  body: {
+    color: "#fff",
+    alignItems: "center",
+    padding: 40,
+    marginBottom: 200,
+  },
+  button: {
+    backgroundColor: "#F86624",
+    padding: 20,
+    width: "90%",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+});
